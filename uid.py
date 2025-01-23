e58afe577c60dc4152778e4ad0640c1d535d59d6
Carbon16 <admin@lgsk.tech>
2025-01-23 19:02:00 +0000

    test

--- uid.py
+++ uid.py
@@ -2,6 +2,8 @@ from mfrc522 import SimpleMFRC522
 reader=SimpleMFRC522()
 try:
     id = reader.read_id()
+    # remove any leading/trailing whitespace
+    id = id.strip()
     print(id)
 finally:
     pass
