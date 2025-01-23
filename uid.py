0ae0f8a432b3c1e6a83ea694c5cfaeabe62f8534
Carbon16 <admin@lgsk.tech>
2025-01-23 18:56:35 +0000

    test

--- uid.py
+++ uid.py
@@ -2,10 +2,6 @@ from mfrc522 import MFRC522
 
 reader = MFRC522()
 status =  None
-while status != reader.MI_OK:
-	(status, TagType) = reader.Request(reader.PICC_REQIDL)
-	if status == reader.MI_OK:
-		print("Connection Success!")
 (status, uid) = reader.Anticoll()
 if status == reader.MI_OK:
 	print(uid)
