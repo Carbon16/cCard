809dcb4e0bc630176875b00f13668a6ba81f9d18
Carbon16 <admin@lgsk.tech>
2025-01-23 18:59:00 +0000

    test

--- uid.py
+++ uid.py
@@ -1,5 +1,5 @@
 from mfrc522 import SimpleMFRC522
 reader=SimpleMFRC522()
 id=reader.read()
-# extract the id from the tuple
-print(id[0])
+# extract only integers from the id
+id = ''.join(filter(str.isdigit, id))
