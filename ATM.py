6d1f5134f7c5a46dd486ffa6392b6f3f632076a8
Carbon16 <admin@lgsk.tech>
2025-01-29 21:56:24 +0000

    push

--- ATM.py
+++ ATM.py
@@ -2,6 +2,13 @@ from mfrc522 import SimpleMFRC522
 reader=SimpleMFRC522()
 import mariadb
 import sys
+from time import sleep
+from RPLCD.i2c import CharLCD
+#lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1, cols=16, rows=2, dotsize=8)
+#lcd.clear()
+#replace "print" with "lcd.write_string" to display on LCD.
+
+
 try:
     conn = mariadb.connect(
         user="sking",
@@ -23,18 +30,34 @@ def read_uid():
     return reader.read_id()
 
 def main():
+    #lcd.clear()
     print("Select an option:\n1. Register\n2. Withdraw\n3. Deposit\n4. Check Balance")
     option = input("Enter option: ")
+    if option == "1":
+        register()
+    elif option == "2":
+        withdraw()
+    elif option == "3":
+        deposit()
+    elif option == "4":
+        check_balance()
+    else:
+        print("Invalid option")
 
 def register():
+    #lcd.clear()
     name = input("Enter name: ")
+    #lcd.clear()
     print("Present card")
     uid = read_uid()
-    cur.execute("INSERT INTO users (name, uid) VALUES (?, ?)", (name, uid))
+    cur.execute("INSERT INTO users (name, uid, credit) VALUES (?, ?, ?)", (name, uid, 350))
     conn.commit()
-    print("User registered")
+    #lcd.clear()
+    print(name + " registered")
+    sleep(3)
 
 def withdraw():
+    #lcd.clear()
     print("Present card")
     uid = read_uid()
     amount = input("Enter amount to withdraw: ")
@@ -43,8 +66,10 @@ def withdraw():
     cur.execute("UPDATE users SET credit=? WHERE uid=?", (credit-int(amount), uid))
     conn.commit()
     print(amount + " withdrawn")
+    sleep(3)
 
 def deposit():
+    #lcd.clear()
     print("Present card")
     uid = read_uid()
     amount = input("Enter amount to deposit: ")
@@ -52,11 +77,19 @@ def deposit():
     credit = cur.fetchone()[0]
     cur.execute("UPDATE users SET credit=? WHERE uid=?", (credit+int(amount), uid))
     conn.commit()
+    #lcd.clear()
     print(amount + " deposited")
+    sleep(3)
 
 def check_balance():
+    #lcd.clear()
     print("Present card")
     uid = read_uid()
     cur.execute("SELECT credit FROM users WHERE uid=?", (uid,))
     credit = cur.fetchone()[0]
+    #lcd.clear()
     print("Balance: " + str(credit))
+    sleep(3)
+
+while True:
+    main()
